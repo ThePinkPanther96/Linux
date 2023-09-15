32c320123184337236af9c1f8da005926b8a3f0e
Gal <gal8156@gmail.com>
2023-09-15 15:45:01 +0300

    added ExecRemoteSSHCommand

---
+++ ExecRemoteSSHCommand.py
@@ -0,0 +1,34 @@
+
+import logging
+import paramiko
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__) 
+
+
+host = "172.31.22.100"
+username = "centos"
+command =  "sudo /root/client.sh chnx75"
+
+def ssh_run_remote(host,username,command):
+
+ port = 22
+
+ ssh = paramiko.SSHClient()
+ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ ssh.connect(host,port, username,key_filename = 'frp.pem')
+ ssh.get_transport()
+
+ stdin, stdout, stderr = ssh.exec_command(command) 
+ out = stdout.read().decode().strip()
+ print(out) 
+ error = stderr.read().decode().strip()
+ print(error)
+ ssh.close()
+ 
+ return out,error 
+
+
+output = ssh_run_remote(host=host,username = username,command = command)
+print (output)
+
+
