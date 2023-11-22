e7df8754d4e71ec84991b7ffbcf01d41d088417d
git <git@192.168.1.51>
2023-11-22 16:03:21 +0200

    audit script

--- Server Tools/File Auditing/file_auditing.sh
+++ Server Tools/File Auditing/file_auditing.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <num_logs> <log_size>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <num_logs> <log_size> <log_file_path>"
     exit 1
 fi
 
 num_logs="$1"
 log_size="$2"
+log_file_path="$3"
 
 # updater server
 sudo apt-get -y update 
@@ -18,4 +19,11 @@ apt-get install -y auditd
 systemctl start auditd
 systemctl enable auditd
 
-# Configure logs
+# Update the configuration file with the new values
+sed -i "s/^num_logs = .*/num_logs = $num_logs/" /etc/audit/auditd.conf
+sed -i "s/^max_log_file = .*/max_log_file = $log_size/" /etc/audit/auditd.conf
+sed -i "s|^log_file = .*|log_file = $log_file_path|" /etc/audit/auditd.conf
+
+# Reset the audit service
+systemctl restart auditd
+
